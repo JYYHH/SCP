237352ee590447b2e1bfa1c64e882e840f41b38d
JYYHH <jin511@purdue.edu>
2024-04-29 10:56:13 +0000

    V0.4.5 debug mode

--- src/util.c
+++ src/util.c
@@ -7,6 +7,7 @@ inline void full_transfer(int in_fd, int out_fd, char *buffer, int type_){
         // decrypt
         while(msg_length = read(in_fd, buffer, MAX_BYTES)){
             // first check integrity
+            printf("%d\n", msg_length);
             int ret = check_integrity((unsigned char *)buffer, &msg_length);
             if (!ret){
                 printf("Encounter the integrity issues...\n");
@@ -22,6 +23,7 @@ inline void full_transfer(int in_fd, int out_fd, char *buffer, int type_){
             padding_((unsigned char *)buffer, &msg_length);
             encrypt((unsigned char *)buffer, msg_length);
             append_HMAC((unsigned char *)buffer, &msg_length);
+            printf("%d\n", msg_length);
             write(out_fd, buffer, msg_length);
         }
     }
